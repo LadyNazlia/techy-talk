1af51de621e40216a964a19086f1853124dc4c0a
Amanda Faulkenberry <89984726+LadyNazlia@users.noreply.github.com>
2022-04-13 05:41:42 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const url = 'https://techy-api.vercel.app/api/json'
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector('h3').innerHTML = `${data.message}`
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
